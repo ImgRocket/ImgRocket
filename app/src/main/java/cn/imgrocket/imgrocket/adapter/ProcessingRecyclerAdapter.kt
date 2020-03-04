83adcaf3f639f5688d40742a5e0ad8b884e93ee9
HHR <467178874@qq.com>
2020-03-04 22:59:34 +0800

    实现基础的点击功能

--- app/src/main/java/cn/imgrocket/imgrocket/adapter/ProcessingRecyclerAdapter.kt
+++ app/src/main/java/cn/imgrocket/imgrocket/adapter/ProcessingRecyclerAdapter.kt
@@ -1,8 +1,13 @@
 package cn.imgrocket.imgrocket.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import cn.imgrocket.imgrocket.APP.Companion.context
 import cn.imgrocket.imgrocket.R
@@ -11,6 +16,7 @@ class ProcessingRecyclerAdapter(private val items: List<String>) : RecyclerView.
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.item_processing, parent, false)
+        view.setOnClickListener(OnClick());
         return ViewHolder(view)
     }
 
@@ -21,6 +27,20 @@ class ProcessingRecyclerAdapter(private val items: List<String>) : RecyclerView.
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var text: TextView = itemView.findViewById(R.id.processing_item_text)
+        var icon: ImageView = itemView.findViewById(R.id.processing_item_icon)
+        var op: ImageView = itemView.findViewById(R.id.processing_item_op)
+        var progress: ProgressBar = itemView.findViewById(R.id.processing_progress)
+
+
+    }
+
+    inner class OnClick : View.OnClickListener {
+
+        override fun onClick(v: View?) {
+            Toast.makeText(context, "哈哈哈", Toast.LENGTH_LONG).show();
+            v!!.findViewById<TextView>(R.id.processing_item_text).text = "123"
+        }
 
     }
 
