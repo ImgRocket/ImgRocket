4b5b6814b1f3f85293d2e2dc2b23473d26cbb876
wcf <1533144693@qq.com>
2020-03-08 22:14:16 +0800

    Update HelpActivity.kt

--- app/src/main/java/cn/imgrocket/imgrocket/HelpActivity.kt
+++ app/src/main/java/cn/imgrocket/imgrocket/HelpActivity.kt
@@ -2,6 +2,8 @@ package cn.imgrocket.imgrocket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.viewpager.widget.ViewPager
 import cn.imgrocket.imgrocket.adapter.SimplePageFragmentAdapter
@@ -12,17 +14,15 @@ import kotlinx.android.synthetic.main.activity_main.main_page_view
 
 class HelpActivity : AppCompatActivity() {
     private lateinit var adapter: SimplePageFragmentAdapter
+    lateinit var helpImgCircles: ArrayList<ImageView>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
         Function.black(this)
         init()
-        help_img_circle1.setImageDrawable(resources.getDrawable(R.drawable.ic_lens_black_24dp, null))
-        help_img_circle2.setImageDrawable(resources.getDrawable(R.drawable.ic_panorama_fish_eye_black_24dp, null))
-        help_img_circle3.setImageDrawable(resources.getDrawable(R.drawable.ic_panorama_fish_eye_black_24dp, null))
-        help_img_circle4.setImageDrawable(resources.getDrawable(R.drawable.ic_panorama_fish_eye_black_24dp, null))
-        help_img_circle5.setImageDrawable(resources.getDrawable(R.drawable.ic_panorama_fish_eye_black_24dp, null))
+        helpImgCircles = arrayListOf(help_img_circle1, help_img_circle2, help_img_circle3, help_img_circle4, help_img_circle5)
+        currentPage(helpImgCircles, 1)
         main_page_view.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageScrollStateChanged(state: Int) {
 
@@ -32,43 +32,7 @@ class HelpActivity : AppCompatActivity() {
             }
 
             override fun onPageSelected(position: Int) {
-                when (position.toString()) {
-                    "0" -> {
-                        help_img_circle1.setImageDrawable(resources.getDrawable(R.drawable.ic_lens_black_24dp, null))
-                        help_img_circle2.setImageDrawable(resources.getDrawable(R.drawable.ic_panorama_fish_eye_black_24dp, null))
-                        help_img_circle3.setImageDrawable(resources.getDrawable(R.drawable.ic_panorama_fish_eye_black_24dp, null))
-                        help_img_circle4.setImageDrawable(resources.getDrawable(R.drawable.ic_panorama_fish_eye_black_24dp, null))
-                        help_img_circle5.setImageDrawable(resources.getDrawable(R.drawable.ic_panorama_fish_eye_black_24dp, null))
-                    }
-                    "1" -> {
-                        help_img_circle1.setImageDrawable(resources.getDrawable(R.drawable.ic_lens_black_24dp, null))
-                        help_img_circle2.setImageDrawable(resources.getDrawable(R.drawable.ic_lens_black_24dp, null))
-                        help_img_circle3.setImageDrawable(resources.getDrawable(R.drawable.ic_panorama_fish_eye_black_24dp, null))
-                        help_img_circle4.setImageDrawable(resources.getDrawable(R.drawable.ic_panorama_fish_eye_black_24dp, null))
-                        help_img_circle5.setImageDrawable(resources.getDrawable(R.drawable.ic_panorama_fish_eye_black_24dp, null))
-                    }
-                    "2" -> {
-                        help_img_circle1.setImageDrawable(resources.getDrawable(R.drawable.ic_lens_black_24dp, null))
-                        help_img_circle2.setImageDrawable(resources.getDrawable(R.drawable.ic_lens_black_24dp, null))
-                        help_img_circle3.setImageDrawable(resources.getDrawable(R.drawable.ic_lens_black_24dp, null))
-                        help_img_circle4.setImageDrawable(resources.getDrawable(R.drawable.ic_panorama_fish_eye_black_24dp, null))
-                        help_img_circle5.setImageDrawable(resources.getDrawable(R.drawable.ic_panorama_fish_eye_black_24dp, null))
-                    }
-                    "3" -> {
-                        help_img_circle1.setImageDrawable(resources.getDrawable(R.drawable.ic_lens_black_24dp, null))
-                        help_img_circle2.setImageDrawable(resources.getDrawable(R.drawable.ic_lens_black_24dp, null))
-                        help_img_circle3.setImageDrawable(resources.getDrawable(R.drawable.ic_lens_black_24dp, null))
-                        help_img_circle4.setImageDrawable(resources.getDrawable(R.drawable.ic_lens_black_24dp, null))
-                        help_img_circle5.setImageDrawable(resources.getDrawable(R.drawable.ic_panorama_fish_eye_black_24dp, null))
-                    }
-                    "4" -> {
-                        help_img_circle1.setImageDrawable(resources.getDrawable(R.drawable.ic_lens_black_24dp, null))
-                        help_img_circle2.setImageDrawable(resources.getDrawable(R.drawable.ic_lens_black_24dp, null))
-                        help_img_circle3.setImageDrawable(resources.getDrawable(R.drawable.ic_lens_black_24dp, null))
-                        help_img_circle4.setImageDrawable(resources.getDrawable(R.drawable.ic_lens_black_24dp, null))
-                        help_img_circle5.setImageDrawable(resources.getDrawable(R.drawable.ic_lens_black_24dp, null))
-                    }
-                }
+                currentPage(helpImgCircles, position + 1)
             }
         })
     }
@@ -77,5 +41,10 @@ class HelpActivity : AppCompatActivity() {
         adapter = SimplePageFragmentAdapter(supportFragmentManager, arrayListOf(HelpFragment1(), HelpFragment2(), HelpFragment3(), HelpFragment4(), HelpFragment5()))
         main_page_view.adapter = adapter
         main_page_view.currentItem = 0
+    }
+
+    private fun currentPage(imgs: ArrayList<ImageView>, pos: Int) {
+        for (i in 0 until pos) imgs[i].setImageDrawable(resources.getDrawable(R.drawable.ic_lens_black_24dp, null))
+        for(i in pos until imgs.size) imgs[i].setImageDrawable(resources.getDrawable(R.drawable.ic_panorama_fish_eye_black_24dp, null))
     }
 }
