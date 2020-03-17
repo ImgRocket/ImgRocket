6d4dff88ec0b17e9e7641bb607e588e0f297f3b0
wcf <1533144693@qq.com>
2020-03-17 19:26:04 +0800

    Update RoundAngleImageView.kt

--- app/src/main/java/cn/imgrocket/imgrocket/widget/RoundAngleImageView.kt
+++ app/src/main/java/cn/imgrocket/imgrocket/widget/RoundAngleImageView.kt
@@ -15,11 +15,7 @@ class RoundAngleImageView : AppCompatImageView {
     private var roundHeight = 5
     private var paint2: Paint? = null
 
-    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
-            context,
-            attrs,
-            defStyle
-    ) {
+    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
         init(context, attrs)
     }
 
@@ -34,16 +30,15 @@ class RoundAngleImageView : AppCompatImageView {
     fun setRound(round: Int) {
         roundHeight = round
         roundWidth = round
+        invalidate()
     }
 
     private fun init(context: Context, attrs: AttributeSet?) {
 
         if (attrs != null) {
             val a = context.obtainStyledAttributes(attrs, R.styleable.RoundAngleImageView)
-            roundWidth =
-                    a.getDimensionPixelSize(R.styleable.RoundAngleImageView_roundWidth, roundWidth)
-            roundHeight =
-                    a.getDimensionPixelSize(R.styleable.RoundAngleImageView_roundHeight, roundHeight)
+            roundWidth = a.getDimensionPixelSize(R.styleable.RoundAngleImageView_roundWidth, roundWidth)
+            roundHeight = a.getDimensionPixelSize(R.styleable.RoundAngleImageView_roundHeight, roundHeight)
             a.recycle()
         } else {
             val density = context.resources.displayMetrics.density
@@ -77,11 +72,7 @@ class RoundAngleImageView : AppCompatImageView {
         path.moveTo(0f, roundHeight.toFloat())
         path.lineTo(0f, 0f)
         path.lineTo(roundWidth.toFloat(), 0f)
-        path.arcTo(
-                RectF(0f, 0f, (roundWidth * 2).toFloat(), (roundHeight * 2).toFloat()),
-                -90f,
-                -90f
-        )
+        path.arcTo(RectF(0f, 0f, (roundWidth * 2).toFloat(), (roundHeight * 2).toFloat()), -90f, -90f)
         path.close()
         canvas.drawPath(path, paint!!)
     }
@@ -91,16 +82,7 @@ class RoundAngleImageView : AppCompatImageView {
         path.moveTo(0f, (height - roundHeight).toFloat())
         path.lineTo(0f, height.toFloat())
         path.lineTo(roundWidth.toFloat(), height.toFloat())
-        path.arcTo(
-                RectF(
-                        0f,
-                        (height - roundHeight * 2).toFloat(),
-                        (roundWidth * 2).toFloat(),
-                        height.toFloat()
-                ),
-                90f,
-                90f
-        )
+        path.arcTo(RectF(0f, (height - roundHeight * 2).toFloat(), (roundWidth * 2).toFloat(), height.toFloat()), 90f, 90f)
         path.close()
         canvas.drawPath(path, paint!!)
     }
@@ -110,14 +92,7 @@ class RoundAngleImageView : AppCompatImageView {
         path.moveTo((width - roundWidth).toFloat(), height.toFloat())
         path.lineTo(width.toFloat(), height.toFloat())
         path.lineTo(width.toFloat(), (height - roundHeight).toFloat())
-        path.arcTo(
-                RectF(
-                        (width - roundWidth * 2).toFloat(),
-                        (height - roundHeight * 2).toFloat(),
-                        width.toFloat(),
-                        height.toFloat()
-                ), 0f, 90f
-        )
+        path.arcTo(RectF((width - roundWidth * 2).toFloat(), (height - roundHeight * 2).toFloat(), width.toFloat(), height.toFloat()), 0f, 90f)
         path.close()
         canvas.drawPath(path, paint!!)
     }
@@ -127,16 +102,7 @@ class RoundAngleImageView : AppCompatImageView {
         path.moveTo(width.toFloat(), roundHeight.toFloat())
         path.lineTo(width.toFloat(), 0f)
         path.lineTo((width - roundWidth).toFloat(), 0f)
-        path.arcTo(
-                RectF(
-                        (width - roundWidth * 2).toFloat(),
-                        0f,
-                        width.toFloat(),
-                        (roundHeight * 2).toFloat()
-                ),
-                -90f,
-                90f
-        )
+        path.arcTo(RectF((width - roundWidth * 2).toFloat(), 0f, width.toFloat(), (roundHeight * 2).toFloat()), -90f, 90f)
         path.close()
         canvas.drawPath(path, paint!!)
     }
